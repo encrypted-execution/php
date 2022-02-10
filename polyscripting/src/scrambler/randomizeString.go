12b7834921232db75deb465c9033517d8cf4cedd
Archis Gore <archis@polyverse.com>
2022-02-10 14:07:45 -0800

    Use cryptographic randomness

--- polyscripting/src/scrambler/randomizeString.go
+++ polyscripting/src/scrambler/randomizeString.go
@@ -4,12 +4,16 @@ package main
 
 import (
 	"crypto/rand"
+	"log"
 	"math/big"
 )
 
 const MAX = 15
 const MIN = 6
 
+// https://stackoverflow.com/a/6878625/6998816
+const MAX_UNIT64 = ^uint64(0)
+const MAX_INT64 = int64(MAX_UNIT64 >> 1)
 const usableChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 const (
@@ -19,14 +23,12 @@ const (
 )
 
 func randomStringGen() string {
-	bigMin := big.NewInt(MIN)
-	bigMax := big.NewInt(MAX)
-	randRage := bigMax.Sub(bigMin)
-	n := rand.Int(rand.Reader, randRage) + bigMin
+	n := cryptoRandInRangeInt64(MIN, MAX)
+
 	b := make([]byte, n)
-	for i, cache, remain := n-1, randSrc.Int63(), letterIdxMax; i >= 0; {
+	for i, cache, remain := n-1, cryptoRantInt64(), letterIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = randSrc.Int63(), letterIdxMax
+			cache, remain = cryptoRantInt64(), letterIdxMax
 		}
 		if idx := int(cache & letterIdxMask); idx < len(usableChars) {
 			b[i] = usableChars[idx]
@@ -37,4 +39,25 @@ func randomStringGen() string {
 	}
 
 	return string(b)
+}
+
+func cryptoRantInt64() int64 {
+	return cryptoRandInRangeInt64(0, MAX_INT64)
+}
+
+func cryptoRandInRangeInt64(min int64, max int64) int64 {
+	bigMin := big.NewInt(min)
+	bigMax := big.NewInt(max)
+	var randRange big.Int
+	randRange.Sub(bigMax, bigMin)
+	randNumInRange, err := rand.Int(rand.Reader, &randRange)
+	if err != nil {
+		log.Fatalf("Unable to generate a cryptographically secure random number: %v")
+	}
+
+	var randNum big.Int
+	randNum.Add(randNumInRange, bigMin)
+
+	// down-cast into 64-bit unsigned int
+	return randNum.Int64()
 }
