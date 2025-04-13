64a05d1b51f14bb31148473297e4a7aefa35eeaa
Archis Gore <me@archisgore.com>
2025-04-13 10:30:44 -0700

    Fixed php image name

--- 8.4/bookworm/apache/publish-image.sh
+++ 8.4/bookworm/apache/publish-image.sh
@@ -3,7 +3,7 @@
 
 PHP_VERSION=8.4
 
-image="ghcr.io/encrypted-execution/php-${PHP_VERSION}-${BUILD_TYPE}-apache-debian"
+image="ghcr.io/encrypted-execution/encrypted-php${PHP_VERSION}-apache-debian"
 
 echo "$(date) Obtaining current git sha for tagging the docker image"
 headsha=$(git rev-parse --verify HEAD)
@@ -14,7 +14,6 @@ echo "Pushing with commit tag..."
 docker push $image:$headsha
 
 if [[ "$1" == "-g" ]]; then
-
     echo "Pushing as latest tag..."
     docker tag $image:$headsha $image:latest
     docker push $image:latest
